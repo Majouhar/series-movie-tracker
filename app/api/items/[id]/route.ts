141a5455cccaed79c696a24b7ca3c68419d5deed
Jouhar <majouharanchalan1998@gmail.com>
2025-08-04 19:07:09 +0530

    next js fix

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -1,10 +1,13 @@
+"use server";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  params: { params: Promise<{ id: string }> }
 ) {
-  const id = parseInt(params.id);
+
+
+  const id = parseInt((await params.params).id);
 
   try {
     await prisma.item.delete({
